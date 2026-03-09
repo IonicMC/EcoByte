3e55babb452404b9d3ca25be2cd481cb6414b968
IonicMC <70986344+IonicMC@users.noreply.github.com>
2026-03-09 11:47:39 +0800

    Update EcoByte.py

--- EcoByte.py
+++ EcoByte.py
@@ -1,27 +1,43 @@
-from ultralytics import YOLO
 import cv2
+import numpy as np
 
-# Load your ONNX model
-model = YOLO("best.onnx")
+print("Loading ONNX model...")
 
-# Open camera
+net = cv2.dnn.readNetFromONNX("best.onnx")
+
+print("Model loaded successfully!")
+
 cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Camera failed to open")
+    exit()
+
+print("Camera opened")
 
 while True:
     ret, frame = cap.read()
+
     if not ret:
+        print("Camera read failed")
         break
 
-    # Run detection
-    results = model(frame)
+    blob = cv2.dnn.blobFromImage(
+        frame,
+        scalefactor=1/255.0,
+        size=(640,640),
+        swapRB=True,
+        crop=False
+    )
 
-    # Draw detections
-    annotated_frame = results[0].plot()
+    net.setInput(blob)
+    outputs = net.forward()
 
-    # Show result
-    cv2.imshow("YOLOv8 Test", annotated_frame)
+    print("Inference ran")
 
-    if cv2.waitKey(1) & 0xFF == ord("q"):
+    cv2.imshow("EcoByte Camera Test", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
